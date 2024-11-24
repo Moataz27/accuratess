6c8b7ef3962b3e0ccc9c08ecd2fe06cdac76c4ed
Moataz <moataz180325@fci.bu.edu.eg>
2024-11-24 12:58:40 +0200

    add description for callback url

--- docs/_app/immutable/entry/start.BRn0IHlx.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.RLvTbgOj.js";export{t as start};
--- magidoc.mjs
+++ magidoc.mjs
@@ -1,29 +1,49 @@
-import path from 'path'
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 export default {
-    introspection: {
-      type: 'sdl',
-      paths: ['./schema.gql'],
-    },
-    website: {
-      template: 'carbon-multi-page',
-      staticAssets: path.join(__dirname, 'assets'),
-      options: {
-        appTitle: 'Accurate Api Documentation',
-        appLogo: '/Accurate.png',
-        siteRoot: '/accuratess',
-        pages: [{
-            title: 'Accurate Api',
-            content: `
-            Accurate Api Documentation`
-        }],
-        queryGenerationFactories: {
-            'Time': '00:00'
-          }
-      },
-    },
-  }
+	introspection: {
+		type: "sdl",
+		paths: ["./schema.gql"],
+	},
+	website: {
+		template: "carbon-multi-page",
+		staticAssets: path.join(__dirname, "assets"),
+		options: {
+			appTitle: "Accurate Api Documentation",
+			appLogo: "/Accurate.png",
+			siteRoot: "/accuratess",
+			pages: [
+				{
+					title: "Accurate Api",
+					content: `
+            Accurate Api Documentation
+            `,
+				},
+				{
+					title: "Callback url",
+					content: `
+            Webhook callback url to listen any update status occurred to shipment via post request
+            -- data returned from callback
+              'shipmentId' => shipment id,
+              'typeCode' => type listen for update,
+              'shipmentStatusCode' => shipment status,
+              'returnTypeCode' => if shipment returned, type of return,
+              'deliveryTypeCode' => if shipment delivered, type of delivery,
+              'deliveredAmount' => amount of delivered,
+              'collectedFees' => amount of collected fees,
+              'deliveryDate' => delivery date for shipment,
+              'cancellationReasonId' => if shipment cancelled, the reason of cancellation,
+              'notes' => notes for shipment,
+            `,
+				},
+			],
+			queryGenerationFactories: {
+				Time: "00:00",
+			},
+		},
+	},
+};
